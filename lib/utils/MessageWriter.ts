98ce202a4f3f1c3870647c728a2bdc8bf97c9b69
2022570 <seungwoo_park2@tmax.co.kr>
2023-04-21 15:40:28 +0900

    docs: 전체 주석 추가

--- lib/utils/MessageWriter.ts
+++ lib/utils/MessageWriter.ts
@@ -19,18 +19,29 @@ batchHeader 0 - 3
 */
 
 // stack 메시지들을 담아 놓고, 1/60 초마다 담은 것을 보낸다.
-// class batch
+// TODO: 1fps 마다 메시지들을 모아서 하나의 batch로 모아줘야 한다. 그에 필요할 것 같은 variables
 const DEFAULT_PATCH_RATE = 1000 / 20; // 20fps (50ms)
 const DEFAULT_SIMULATION_INTERVAL = 1000 / 60; // 60fps (16.66ms)
 
 export class MessageWriter {
+  // TODO: 1fps 마다 메시지들을 모아서 하나의 batch로 모아줘야 한다. 그에 필요할 것 같은 variables
   #patchInterval: boolean = false;
   #delay: number = DEFAULT_SIMULATION_INTERVAL;
 
+  /**
+   * @brief message에 적힐 현재 offset. 초기 값 0
+   */
   #offset: number = protocol.eOffsetManager.INIT_OFFSET;
 
+  /**
+   * @brief 하나의 message Buffer
+   */
   #message: Buffer;
 
+  /**
+   * @brief 메시지 헤더(message id(4bytes), message length(4bytes)) write
+   * @param _msgID 프로토콜 메시지 id.
+   */
   #setMessageHeader(_msgID: number) {
     this.#offset = this.#message.writeUInt32LE(_msgID, this.#offset);
     this.#offset = this.#message.writeUInt32LE(
@@ -41,9 +52,14 @@ export class MessageWriter {
     );
   }
 
+  /**
+   * @brief register object나 sync object할 때 message body write
+   * @param _params SyncType { syncObjID: number; attributes: { [key: number]: number }; }
+   */
   createObjectMessage(_params: SyncType) {
     const isInstanceID = _params.syncObjID > 0x00010000;
 
+    // instanceID가 부여 되어 있으면 sync하는 것으로 간주
     isInstanceID
       ? (this.#offset = this.#message.writeUInt32LE(
           _params.syncObjID,
@@ -54,6 +70,7 @@ export class MessageWriter {
           this.#offset + 2
         )); // prefab ID만 있을 때
 
+    // object의 key들을 돌면서 해당 attribute들 직렬화
     for (const key in _params.attributes) {
       const value: number = _params.attributes[key];
       this.#offset = this.#message.writeUInt32LE(
@@ -68,6 +85,11 @@ export class MessageWriter {
     }
   }
 
+  /**
+   * @brief object regist 할 때 message body 부분의 총 길이가 얼마나 나올지 계산
+   * @param params SyncType
+   * @returns message body가 적힐 총 길이
+   */
   #getArrayParamsLength(params: SyncType[]) {
     let messageBodyLength = 0;
     for (const param of params) {
@@ -81,11 +103,16 @@ export class MessageWriter {
     return messageBodyLength;
   }
 
+  /**
+   * @brief 메시지 생성
+   * @param _msgID 프로토콜 메시지 id.
+   * @param params number일 경우 4bytes의 message body만을 생성. SyncType일 경우 sync 하는 것으로 간주. SyncType[] 일 경우 regist object 하는 것으로 간주
+   */
   #createMessage(_msgID: number, params?: number | SyncType | SyncType[]) {
     let len =
       protocol.eOffsetManager.MESSAGE_HEADER_LENGTH +
       protocol.eOffsetManager.BATCH_HEADER_LENGTH;
-    // 객체 생성
+    // 객체 등록
     if (Array.isArray(params)) {
       len += this.#getArrayParamsLength(params);
       this.#message = Buffer.alloc(len);
@@ -122,10 +149,16 @@ export class MessageWriter {
     }
   }
 
+  /**
+   * @brief 현재 offset 0으로 초기화
+   */
   #initOffset() {
     this.#offset = protocol.eOffsetManager.INIT_OFFSET;
   }
 
+  /**
+   * @brief batch의 헤더에 batch 총 길이 write. 현재 batch의 총 길이는 현재 offset - 4(batch header length)
+   */
   #setBatchHeader() {
     this.#message.writeUInt32LE(
       this.#offset - protocol.eOffsetManager.BATCH_HEADER_LENGTH,
@@ -133,6 +166,13 @@ export class MessageWriter {
     );
   }
 
+  // TODO: message를 return 하는 것이 아니라 1fps 마다 message들 모아서 batch 생성하고 해당 batch return 해야 함.
+  /**
+   * @brief batch 세팅
+   * @param _msgID 프로토콜 메시지 id.
+   * @param params number일 경우 4bytes의 message body만을 생성. SyncType일 경우 sync 하는 것으로 간주. SyncType[] 일 경우 regist object 하는 것으로 간주
+   * @returns write된 message
+   */
   setBatch(_msgID: number, _params?: number | SyncType | SyncType[]) {
     this.#createMessage(_msgID, _params);
     this.#setBatchHeader();
--- lib/utils/SyncManager.ts
+++ lib/utils/SyncManager.ts
@@ -20,6 +20,7 @@ export class SyncManager extends MessageWriter {
   onOtherClientJoined: (res: OtherClientJoined) => any;
 
   /**
+   *
    * @param _url websocket url
    */
   constructor(_url: string) {
@@ -46,7 +47,9 @@ export class SyncManager extends MessageWriter {
     });
   }
 
-  // 서버로부터 응답 받기
+  /**
+   * @brief 서버로부터 응답 받기
+   */
   subscribe() {
     const subscribe = (e: any) => {
       this.#ReadProtocol(e);
@@ -55,7 +58,10 @@ export class SyncManager extends MessageWriter {
     this.socket.addEventListener("message", subscribe);
   }
 
-  // 서버로부터 받은 응답 해석
+  /**
+   * @brief 서버로부터 받은 응답 해석
+   * @param e 서버로부터 받은 binary code
+   */
   #ReadProtocol(e: any) {
     let dataView = new DataView(e.data, 0);
     let offset = 0;
@@ -65,9 +71,9 @@ export class SyncManager extends MessageWriter {
 
     let offsetBound = 0;
     while (offset < tcpHeader) {
+      // MessageID
       const messageID = dataView.getUint32(offset, true);
       offset += protocol.eOffsetManager.MESSAGE_ID_LENGTH;
-      // MessageID
       offsetBound = offsetBound + dataView.getUint32(offset, true) + 8;
       offset += protocol.eOffsetManager.MESSAGE_LEN_LENGTH;
 
@@ -86,6 +92,9 @@ export class SyncManager extends MessageWriter {
         // 룸 connect 응답
         case protocol.eMessageID.ROOM_JOIN_RES:
           const sceneId: number = Number(serverData[3]);
+          if (sceneId === 0xffffffff) {
+            throw new Error("존재하지 않는 방에 대한 입장 요청입니다.");
+          }
           this.#checkCallback(this.onJoinRoom, "onJoinRoom 콜백을 입력하세요");
           this.onJoinRoom(sceneId);
           offset += protocol.eOffsetManager.OFFSET_LENGTH;
@@ -131,6 +140,7 @@ export class SyncManager extends MessageWriter {
           return new Promise((resolve, reject) => {
             resolve(roomList);
           });
+        // 특정 룸 정보
         case protocol.eMessageID.ROOM_INFO_RES:
           const roomInfo = this.#getRoomInfo(offset, dataView, offsetBound);
           return new Promise((resolve, reject) => {
@@ -155,6 +165,11 @@ export class SyncManager extends MessageWriter {
     }
   }
 
+  /**
+   * @brief  다른 클라이언트의 입장에 대한 응답
+   * @params offset: 현재까지 증가했던 오프셋, dataView
+   * @returns binary code가 해석된 offset 증가되고, 그 증가된 offset 반환
+   */
   #getOtherClientJoined(offset: number, dataView: any): number {
     const clientType = dataView.getUint32(offset, true);
     offset += protocol.eOffsetManager.OFFSET_LENGTH;
@@ -182,17 +197,20 @@ export class SyncManager extends MessageWriter {
   }
 
   /**
-   * 룸 리스트 보내기
+   * @brief  룸 리스트 보내기
    * @params offset: 현재까지 증가했던 오프셋, dataView, offsetBound: 현재까지 오프셋바운드
-   * @type number
+   * @returns 룸 정보(Room Id, Room Name, Player Number)를 리스트로 반환
    */
   #getRoomList(offset: number, dataView: any, offsetBound: number) {
     let roomList: any[] = [];
     let tempList: any = new Object();
 
     while (offset < 4 + offsetBound) {
+      // Room Id
       let roomID: number = dataView.getUint32(offset, true);
       offset += protocol.eOffsetManager.OFFSET_LENGTH;
+
+      // Room Name
       let byteView = new Uint8Array(dataView.buffer, offset, 80);
       let charCodes = Array.from(byteView).map((byte) =>
         String.fromCharCode(byte)
@@ -201,7 +219,7 @@ export class SyncManager extends MessageWriter {
       let roomName = this.#removeInvalidCharacters(stringChar);
       offset += 80;
 
-      // TODO: 준엽님 player Number 숫자 제대로 안나와요 ㅠ
+      // Player Number
       let playerNumber: number = dataView.getUint32(offset, true);
       offset += protocol.eOffsetManager.OFFSET_LENGTH;
 
@@ -213,7 +231,6 @@ export class SyncManager extends MessageWriter {
       roomList.push(tempList);
     }
     // RoomList
-
     return roomList;
   }
 
@@ -224,10 +241,11 @@ export class SyncManager extends MessageWriter {
   }
 
   /**
-   * 특정 룸 정보 보내기
+   * @brief  특정 룸 정보
    * @params offset: 현재까지 증가했던 오프셋, dataView, offsetBound: 현재까지 오프셋바운드
-   * @type number
+   * @returns 특정 룸 정보(Room Id, Room Name, Player Number, Player List)를 리스트로 반환
    */
+
   #getRoomInfo(offset: number, dataView: any, offsetBound: number): number {
     let roomInfo: any = new Object();
 
@@ -279,9 +297,9 @@ export class SyncManager extends MessageWriter {
   }
 
   /**
-   *객체 생성 보내기
+   * @brief 객체 생성 보내기
    * @params offset: 현재까지 증가했던 오프셋, dataView, offsetBound: 현재까지 오프셋바운드
-   * @type number
+   * @returns binary code에서 해석된 offset 만큼 반환
    */
   #getCreateObj(offset: number, dataView: any, offsetBound: number): number {
     let ObjId = dataView.getUint32(offset, true);
@@ -316,6 +334,7 @@ export class SyncManager extends MessageWriter {
    *싱크 보내기
    * @params offset: 현재까지 증가했던 오프셋, dataView, offsetBound: 현재까지 오프셋바운드
    * @type number
+   * @returns binary code가 해석된 offset 증가되고, 그 증가된 offset 반환
    */
   #getSync(offset: number, dataView: any, offsetBound: number): number {
     let syncObjId = dataView.getUint32(offset, true);
@@ -340,7 +359,6 @@ export class SyncManager extends MessageWriter {
   }
 
   /**
-   *
    * @returns number : your connection ID
    */
   getConnectionID() {
@@ -422,36 +440,20 @@ export class SyncManager extends MessageWriter {
     );
   }
 
+  /**
+   * @details 특정 룸 정보를 요청한다.
+   * @param roomNumber
+   * @param cb
+   */
   reqRoomInfo(roomNumber: number, cb: Function) {
-    const isWebSocketOpen = this.socket.readyState === WebSocket.OPEN;
-    if (isWebSocketOpen) {
+    // connectionID가 0이 아니면 이미 웹소캣이 연결 되어 있는 것.
+    if (this.#connectionID !== 0) {
       this.socket.send(
         this.setBatch(protocol.eMessageID.ROOM_INFO_REQ, roomNumber)
       );
-    } else {
-      this.socket.onopen = () => {
-        this.socket.send(this.setBatch(protocol.eMessageID.ROOM_LIST_REQ));
-      };
     }
-
-    const getRoomInfo = (e: any) => {
-      const response = this.#ReadProtocol(e);
-      if (typeof response !== "number" && typeof response !== "undefined") {
-        response.then((res) => {
-          response instanceof Object && cb(response);
-          this.socket.removeEventListener("message", getRoomInfo);
-        });
-      }
-    };
-
-    this.socket.addEventListener("message", getRoomInfo);
-  }
-
-  reqRoomListInfo(cb: Function) {
-    const isWebSocketOpen = this.socket.readyState === WebSocket.OPEN;
-    if (isWebSocketOpen) {
-      this.socket.send(this.setBatch(protocol.eMessageID.ROOM_LIST_REQ));
-    } else {
+    // connectionID 값이 바뀌어야, 그 때 특정 룸 정보 요청.
+    else {
       this.socket.onopen = () => {
         const interval = setInterval(() => {
           if (this.#connectionID !== 0) {
@@ -462,10 +464,51 @@ export class SyncManager extends MessageWriter {
       };
     }
 
-    const getRoomListInfo = (e: any) => {
+    const getRoomInfo = (e: any) => {
       const response = this.#ReadProtocol(e);
+      // ReadProtocol로 받은 것이 promise면, getRoomInfo 등록한 이벤트 삭제
       if (typeof response !== "number" && typeof response !== "undefined") {
         response.then((res) => {
+          // 받은 res 값 cb으로 돌려준다.
+          response instanceof Object && cb(res);
+          this.socket.removeEventListener("message", getRoomInfo);
+        });
+      }
+    };
+
+    this.socket.addEventListener("message", getRoomInfo);
+  }
+
+  /**
+   *
+   * @param cb
+   */
+  reqRoomListInfo(cb: Function) {
+    // connectionID가 0이 아니면 이미 웹소캣이 연결 되어 있는 것.
+    if (this.#connectionID !== 0) {
+      this.socket.send(this.setBatch(protocol.eMessageID.ROOM_LIST_REQ));
+    }
+    // connectionID 값이 바뀌어야, 그 때 룸 리스트 요청.
+    else {
+      this.socket.onopen = () => {
+        const interval = setInterval(() => {
+          if (this.#connectionID !== 0) {
+            this.socket.send(this.setBatch(protocol.eMessageID.ROOM_LIST_REQ));
+            clearInterval(interval);
+          }
+        }, 60);
+      };
+    }
+
+    /**
+     * @param e evnet
+     */
+    const getRoomListInfo = (e: any) => {
+      const response = this.#ReadProtocol(e);
+      // ReadProtocol로 받은 것이 promise면, getRoomListInfo로 등록한 이벤트 삭제
+      if (typeof response !== "number" && typeof response !== "undefined") {
+        response.then((res) => {
+          // 받은 res 값 cb으로 돌려준다.
           Array.isArray(res) && cb(res);
           this.socket.removeEventListener("message", getRoomListInfo);
         });
@@ -513,6 +556,7 @@ export class SyncManager extends MessageWriter {
    * session close
    */
   closeConnection() {
+    this.#connectionID = 0;
     this.socket.close();
   }
 }
